3a6d6d2c494d884f8b15fc2fe0e94ca36162f9aa
fennz6123 <anlinh6123@gmail.com>
2023-03-07 23:35:39 +0700

    aa

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Chị em 🥳';
+const giftUrl = '';
+const eventName = 'Chúc Mừng 08-03';
+const titleCard = 'Tặng chị em CX Team';
+const contentCard = 'Nhân ngày 08-03 🎉Chúc chị em team chúng mình thật xinh đẹp, rạng rỡ và hạnh phúc nha💓';
+
+// phần dưới dành c ho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'img1.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
